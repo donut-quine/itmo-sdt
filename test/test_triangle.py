94b4f269d8a210f4d768a43baa9c78923150a5ea
Danila <crowo4ka@gmail.com>
2024-11-13 17:24:08 +0300

    refactor(triangle): added new test with triangle inequality

--- test/test_triangle.py
+++ test/test_triangle.py
@@ -9,16 +9,21 @@ class TriangleTestCase(unittest.TestCase):
         self.assertRaises(NotPositiveLengthException, lambda: area(-1, 123))
 
     def test_negative_perimeter(self):
-        self.assertRaises(NotPositiveLengthException, lambda: perimeter(123, -1, 1))
+        self.assertRaises(NotPositiveLengthException, lambda: perimeter(3, 4, -5))
 
     def test_zero_area(self):
         self.assertRaises(NotPositiveLengthException, lambda: area(0, 0))
 
     def test_zero_perimeter(self):
-        self.assertRaises(NotPositiveLengthException, lambda: perimeter(0, 1, 2))
+        self.assertRaises(NotPositiveLengthException, lambda: perimeter(3, 4, 0))
+
+    # Не соответствует неравенству треугольника
+    def test_wrong_triangle_perimeter(self):
+        self.assertRaises(Exception, lambda: perimeter(3, 4, 80))
     
     def test_area(self):
         self.assertEqual(6, area(3, 4))
     
     def test_perimeter(self):
         self.assertEqual(12, perimeter(3, 4, 5))
+    
